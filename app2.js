17e6b6aa5738a40fc6dba7b33bb80386574c6b32
vyshnavi1996 <vyshu.tadipaneni14@gmail.com>
2021-03-17 11:16:10 -0500

    adding bonus.html and response.html

--- app2.js
+++ app2.js
@@ -1,11 +1,11 @@
 const http = require('http')    //Pull in a useful node package
-                                
+
 const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
 const port = process.env.port || 3001               //and the port
 
 const server =
   http.createServer(            //Creates the response loop
-    (req,res)=> {               //Anonymous function to handle the request
+    (req, res) => {               //Anonymous function to handle the request
       res.statusCode = 200      //code for OK
       res.setHeader('Content-Type', 'text/html') //Set the mime type HTML
 
@@ -20,8 +20,8 @@ const server =
       res.write('</table>')
       res.end('</body></html>')
       //Close the response
-    }                           
-)
+    }
+  )
 
 server.listen(port, hostname, () => {   //Start the server
   console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
